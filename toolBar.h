f52ccabbb5bfa76e49ef09256dd32eb8190395ba
Sergey <Alamuth@mail.ru>
2020-09-18 21:11:42 +0300

    Lession7

---
+++ toolBar.h
@@ -0,0 +1,14 @@
+#ifndef TOOLBAR_H
+#define TOOLBAR_H
+
+#include <QObject>
+#include <QWidget>
+
+class toolBar : public QMenuBar
+{
+    Q_OBJECT
+public:
+    toolBar();
+};
+
+#endif // TOOLBAR_H
